768b8da0f6e9acb7d93b31e9497769fae156517b
gunjit <gunjitmittal1@gmail.com>
2022-08-14 22:35:06 +0530

    Environment changes

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ import os
 files = dotenv.load_dotenv(".env")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # #CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
